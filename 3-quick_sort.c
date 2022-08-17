7013a2ff428af94b627eec4bb4e5a19e58277b5d
codestronomer <johnrumide6@gmail.com>
2022-08-17 02:49:45 +0100

    [fix] remove exe, correct betty syntax errors

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -9,82 +9,82 @@
  */
 void swap(int a[], int i, int j)
 {
-        int temp;
+	int temp;
 
-        temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
+	temp = a[i];
+	a[i] = a[j];
+	a[j] = temp;
 }
 
 /**
  * partition - partition the array and get the pivot index
  *
  * @a: array to be partitioned
- * @lo - first index
- * @hi - last index
- * @size - size of the array @array
+ * @lo: first index
+ * @hi: last index
+ * @size: size of the array @array
  * Return: partiion index
  */
 int partition(int a[], int lo, int hi, size_t size)
 {
-        /* choose the last element as the pivot */
-        int pivot = hi;
-        /* temporary pivot index */
-        int i = lo;
-        int j;
+	/* choose the last element as the pivot */
+	int pivot = hi;
+	/* temporary pivot index */
+	int i = lo;
+	int j;
 
-        for (j = lo; j < hi; j++)
-        {
-                /* If the current element is less than or equal to the pivot*/
-                if (a[j] <= a[pivot])
-                {
-                        /* Move the temporary pivot index forward */
-                        if (j != i)
-                        {
-                                /* swap the current element with the element at the
-                                temporary pivot index */
-                                swap(a, i, j);
-                                print_array(a, size);
-                        }
-                        i++;
-                }
-        }
+	for (j = lo; j < hi; j++)
+	{
+		/* If the current element is less than or equal to the pivot*/
+		if (a[j] <= a[pivot])
+		{
+			/* Move the temporary pivot index forward */
+			if (j != i)
+			{
+				/* swap the current element with the element at the */
+				/* temporary pivot index */
+				swap(a, i, j);
+				print_array(a, size);
+			}
+			i++;
+		}
+	}
 
-        /* move the pivot element to the correct pivot position */
-        if (a[pivot] < a[i])
-        {
-                swap(a, pivot, i);
-                print_array(a, size);
-        }
-        return (i);
+	/* move the pivot element to the correct pivot position */
+	if (a[pivot] < a[i])
+	{
+		swap(a, pivot, i);
+		print_array(a, size);
+	}
+	return (i);
 }
 
 /**
  * QuickSort - sorts a portion of the array, diveides it into
  * partitions, then sorts those
  *
- * @array: array to be sorted
+ * @a: array to be sorted
  * @lo: first index of the array or subarray
  * @hi: last index of the array or subarray
  * @size: size of the array @array
  */
 void QuickSort(int a[], int lo, int hi, size_t size)
 {
-        int p;
+	int p;
 
-        /* check if indices are correct order */
-        if (lo >= hi || lo < 0)
-                return;
+	/* check if indices are correct order */
+	if (lo >= hi || lo < 0)
+		return;
 
-        /* partition array and get the pivot index */
-        if (lo < hi)
-        {
-                p = partition(a, lo, hi, size);
+	/* partition array and get the pivot index */
+	if (lo < hi)
+	{
+		p = partition(a, lo, hi, size);
 
-                /* sort the two partitions */
-                QuickSort(a, lo, p - 1, size);
-                QuickSort(a, p + 1, hi, size);
-        }
+		/* sort the two partitions */
+		QuickSort(a, lo, p - 1, size);
+		QuickSort(a, p + 1, hi, size);
+	}
 }
 
 /**
@@ -92,12 +92,12 @@ void QuickSort(int a[], int lo, int hi, size_t size)
  * using the Quick sort algorithm
  *
  * @array: array to be sorted by the algorithm
- * size: Size of the array @array
+ * @size: Size of the array @array
  */
 void quick_sort(int *array, size_t size)
 {
-        if (array == NULL || size < 2)
-                return;
+	if (array == NULL || size < 2)
+		return;
 
-        QuickSort(array, 0, size - 1, size);
+	QuickSort(array, 0, size - 1, size);
 }
